4dd5e41c6461f3625c281dfdd83b8c3bd1187c5d
Felix <twchenfeng@gmail.com>
2024-10-27 17:19:35 +0800

    components and layouts

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class = "bg-sky-100 py-2">
+        <p class = "px-6 py-4 text-2xl text-gray-700">預設佈局</p>
+        <slot name = "header"/>
+        <slot/>
+        <slot name = "footer"/>
+    </div>
+</template>
